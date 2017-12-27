20e7be68596e4e2cd8aa8a0e1b776b1f97290039
tianyuningmou <tianyuningmou2009@126.com>
2017-12-27 15:41:07 +0800

    what is pytorch、

---
+++ 01_start.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright () 2017
+
+All rights reserved by easyto
+
+FILE:  01_start.py
+AUTHOR:  tianyuningmou
+DATE CREATED:  @Time : 2017/12/27 下午3:40
+
+DESCRIPTION:  .
+
+VERSION: : #1 $
+CHANGED By: : tianyuningmou $
+CHANGE:  :  $
+MODIFIED: : @Time : 2017/12/27 下午3:40
+"""
+
+
