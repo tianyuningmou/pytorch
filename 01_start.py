5a8112ea775eecb700ff74a015b77243d2c30f6d
tianyuningmou <tianyuningmou2009@126.com>
2018-01-12 17:00:42 +0800

    pytorch start

--- 01_start.py
+++ 01_start.py
@@ -19,9 +19,20 @@ MODIFIED: : @Time : 2017/12/27 下午3:40
 
 from __future__ import print_function
 import torch
+import numpy as np
 
+# 构建模型
 x = torch.Tensor(5, 3)
 y = torch.rand(5, 3)
 z = torch.rand(5, 3)
-print(x, '\n', y, '\n', x.size(), '\n', y+z)
 
+# from pytorch turn to numpy
+a = torch.ones(5)
+b = a.numpy()
+
+# from numpy turn to pytorch
+
+
+# print(x, '\n', y, '\n', x.size(), '\n', y+z)
+# print(torch.add(y, z), '\n', y.add_(z))
+print(a, '\n', b)
---
+++ 02_Tensor.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright () 2018
+
+All rights reserved by easyto
+
+FILE:  02_Tensor.py
+AUTHOR:  tianyuningmou
+DATE CREATED:  @Time : 2018/1/11 下午12:09
+
+DESCRIPTION:  .
+
+VERSION: : #1 $
+CHANGED By: : tianyuningmou $
+CHANGE:  :  $
+MODIFIED: : @Time : 2018/1/11 下午12:09
+"""
+
+import torch
+
+a =
