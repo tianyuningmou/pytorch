f8517f9b672006295ae44b97eca0d359e1479875
tianyuningmou <tianyuningmou2009@126.com>
2018-01-15 14:21:39 +0800

    03_Variable

---
+++ 03_Variable.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright () 2018
+
+All rights reserved by easyto
+
+FILE:  03_Variable.py
+AUTHOR:  tianyuningmou
+DATE CREATED:  @Time : 2018/1/15 下午1:52
+
+DESCRIPTION:  .
+
+VERSION: : #1 $
+CHANGED By: : tianyuningmou $
+CHANGE:  :  $
+MODIFIED: : @Time : 2018/1/15 下午1:52
+"""
+
+
