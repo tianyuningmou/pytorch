c19b984c528f79209b9877631cd0743dbf2ef283
tianyuningmou <tianyuningmou2009@126.com>
2018-01-15 16:04:07 +0800

    05_Module

--- 05_Module.py
+++ 05_Module.py
@@ -3,7 +3,7 @@
 """
 Copyright () 2018
 
-All rights reserved by easyto
+All rights reserved by tianyuningmou
 
 FILE:  05_Module.py
 AUTHOR:  tianyuningmou
@@ -17,4 +17,14 @@ CHANGE:  :  $
 MODIFIED: : @Time : 2018/1/15 下午3:29
 """
 
+from torch import nn
 
+
+class NetName(nn.Module):
+    def __init__(self, other_arguments):
+        super(NetName, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels='', out_channels='', kernel_size='')
+
+    def forward(self, x):
+        x = self.conv1(x)
+        return x
