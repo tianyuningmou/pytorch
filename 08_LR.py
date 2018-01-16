4832562b18758052609d1f8eb97937fbbfbae2e1
tianyuningmou <tianyuningmou2009@126.com>
2018-01-16 15:43:33 +0800

    一维线性回归模型

---
+++ 08_LR.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright () 2018
+
+All rights reserved by easyto
+
+FILE:  08_LR.py
+AUTHOR:  tianyuningmou
+DATE CREATED:  @Time : 2018/1/16 下午2:52
+
+DESCRIPTION:  .
+
+VERSION: : #1 $
+CHANGED By: : tianyuningmou $
+CHANGE:  :  $
+MODIFIED: : @Time : 2018/1/16 下午2:52
+"""
+
+import numpy as np
+from matplotlib import pyplot as plt
+import torch
+from torch import nn
+from torch import optim
+from torch.autograd import Variable
+
+x_train = np.array([[3.3], [4.4], [5.5], [6.71], [6.93], [4.168],
+                    [9.779], [6.182], [7.59], [2.167], [7.042],
+                    [10.791], [5.313], [7.997], [3.1]], dtype=np.float32)
+
+y_train = np.array([[1.7], [2.76], [2.09], [3.19], [1.694], [1.573],
+                    [3.366], [2.596], [2.53], [1.221], [2.827],
+                    [3.465], [1.65], [2.904], [1.3]], dtype=np.float32)
+
+# 散点图
+plt.scatter(x_train, y_train)
+# 折线图
+plt.plot(x_train, y_train)
+# 显示
+plt.show()
+
+x_train = torch.from_numpy(x_train)
+y_train = torch.from_numpy(y_train)
+
+# 定义一维线性回归模型
+class LinearRegression(nn.Module):
+    def __init__(self):
+        super(LinearRegression, self).__init__()
+        self.linear = nn.Linear(1, 1)
+
+    def forward(self, x):
+        out = self.linear(x)
+        return out
+
+model = LinearRegression()
+criterion = nn.MSELoss()
+optimizer = optim.SGD(model.parameters(), lr=1e-3)
+
+# 训练模型
+num_epochs = 1000
+for epoch in range(num_epochs):
+    inputs = Variable(x_train)
+    target = Variable(y_train)
+    # 正向传播
+    out = model(inputs)
+    loss = criterion(out, target)
+    # 反向传播
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    if (epoch+1) % 20 == 0:
+        print('Epoch[{}/{}], loss:{}'.format(epoch+1, num_epochs, loss.data[0]))
+
+model.eval()
+predict = model(Variable(x_train))
+predict = predict.data.numpy()
+plt.plot(x_train.numpy(), y_train.numpy(), 'ro', label='Original data')
+plt.plot(x_train.numpy(), predict, label='Fitting Line')
+plt.show()
