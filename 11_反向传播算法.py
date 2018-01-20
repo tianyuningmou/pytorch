b7cb731b5661bc221eefc1981b11cbbb3bf587ab
tianyuningmou <tianyuningmou2009@126.com>
2018-01-20 17:19:51 +0800

    11

---
+++ 11_反向传播算法.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright () 2018
+
+All rights reserved by easyto
+
+FILE:  11_反向传播算法.py
+AUTHOR:  tianyuningmou
+DATE CREATED:  @Time : 2018/1/19 下午6:21
+
+DESCRIPTION:  .
+
+VERSION: : #1 $
+CHANGED By: : tianyuningmou $
+CHANGE:  :  $
+MODIFIED: : @Time : 2018/1/19 下午6:21
+"""
+
+
